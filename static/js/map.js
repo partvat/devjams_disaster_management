0a3f68dbdbb68aa5e1226cbc1491b7819bafe2e8
Dumblysmart <dumblysmart@proton.me>
2025-09-21 20:56:27 +0530

    adding the recuer feature by Akil

---
+++ static/js/map.js
@@ -0,0 +1,10 @@
+// Initialize map
+const urlParams = new URLSearchParams(window.location.search);
+const role = urlParams.get('role');
+
+const map = L.map("map").setView([20.5937, 78.9629], 5);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; OpenStreetMap contributors',
+}).addTo(map);
+
